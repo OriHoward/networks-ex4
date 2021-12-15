95e965a67fac869aec656b7e6de9a22d57a5c436
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2021-12-16 00:49:49 +0200

    Update sender.c
    Final sender?

--- cmake-build-debug/sender.c
+++ cmake-build-debug/sender.c
@@ -18,7 +18,7 @@ socklen_t len;
 int totalBytes = 0;
 struct sockaddr_in server;
 
-void send_file(FILE *fp, int sock) {
+void send_file() {
     char data[256] = {0};
 
     while (fgets(data, 256, fp) != NULL) {
@@ -32,7 +32,7 @@ void send_file(FILE *fp, int sock) {
 
 
 int main(int argc, char **argv) {
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sock < 0) {
         perror("socket");
         return -1;
@@ -52,41 +52,35 @@ int main(int argc, char **argv) {
         perror("reading file");
         exit(1);
     }
-
+    printf("sending first time:\n");
     for (int i = 0; i < 5; ++i) {
-        send_file(fp, sock);
+        send_file();
+        printf("Successfully send the %d time!\n", i);
     }
     printf("first send successful");
 
-    len = sizeof(buf);
-
-    //cubic
-    if (getsockopt(sock, IPPROTO_IP, TCP_CONGESTION, buf, &len) != 0) {
-        perror("getsockopt");
-        return -1;
-    }
-    puts(buf);
-    printf("Current: %s\n", buf);
-
-    //reno
+    //changing to reno
     strcpy(buf, "reno");
     len = strlen(buf);
-    if (setsockopt(sock, IPPROTO_IP, TCP_CONGESTION, buf, len) != 0) {
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
         perror("setsockopt");
         return -1;
     }
-
-    //send file
+    printf("Changed to %s", buf);
+    //new read of the file
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("reading file");
+        exit(1);
+    }
+    printf("Sending second time:\n");
+    //send file second time
     for (int i = 0; i < 5; ++i) {
-        totalBytes += send(sock, fgets(buf, 256, fp), 256, 0);
+        send_file();
+        printf("Successfully sent the file on the %d time!\n", i);
     }
 
     len = sizeof(buf);
-    if (getsockopt(sock, IPPROTO_IP, TCP_CONGESTION, buf, &len) != 0) {
-        perror("getsockopt");
-        return -1;
-    }
-    printf("New: %s\n", buf);
     close(sock);
     return 0;
 }
